a5ce7f853952856ed32657686cfb90cbbc96f28c
ryoheikamata <r.k.pitschi@gmail.com>
2023-01-08 19:04:17 +0900

    update settings.py

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR/'.env')
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'nennencorori', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ryoheikamata.pythonanywhere.com']
 
 
 # Application definition
